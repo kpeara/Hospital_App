f499b152e94a193da998fceca0a8f1622d992e40
Kunjunnithan Giri Perath <kperath@uwo.ca>
2019-11-10 23:35:16 +0000

    started home page, created titlebar, navbar

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
+<?php
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Doctor</title>
+    <title>HPDR</title>
+    <style type="text/css">
+    body {
+        margin: 0px;
+    }
+    .title {
+        font-size: 50px;
+    }
+    .subtitle {
+        font-size: 18px;
+        padding-left: 20px;
+    }
+    .titlebar {
+        background-color: #3F67E8;
+        padding: 20px;
+        text-align: center;
+        color: white;
+    }
+    nav {
+        background-color: black;
+        color: white;
+        padding: 10px;
+        text-align: center;
+    }
+    </style>
 </head>
 <body>
+<div class="titlebar">
+<span class="title">HDPR</span>
+<span class="subtitle">The Hospital Doctor-Patient Record Management System</span>
+</div>
+<nav>HOME</nav>
     
 </body>
 </html>
