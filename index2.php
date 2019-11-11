e96eefa2c2990dcc433812c56eac28c2c634374a
Kunju Perath <kperath@gmail.com>
2019-11-10 19:21:59 -0500

    created separated files, connected to db

---
+++ index2.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HPDR</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"/>
+</head>
+<body>
+<?php include("templates/header.php"); ?>
+<div class="content">
+<?php
+include("dbconnect.php");
+?>
+</div>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,5 @@
+<div class="titlebar">
+<span class="title">HDPR</span>
+<span class="subtitle">The Hospital Doctor-Patient Record Management System</span>
+</div>
+<nav>DOCTORS</nav>
