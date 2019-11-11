8f1c7569e9f2acc347316102cac4f8993d19684f
Kunjunnithan Giri Perath <kperath@uwo.ca>
2019-11-11 03:21:32 +0000

    finished form

--- index.php
+++ index.php
@@ -10,20 +10,27 @@
 <body>
 <?php include("templates/header.php"); ?>
 <div class="content">
-<h3> View Current Doctors </h3>
+<h2> View Current Doctors </h2>
 <table>
-<tr style="background-color: lightblue;">
-    <th>First Name</th>
-    <th>Last Name</th>
-</tr>
 <?php
 
 include("dbconnect.php");
 
-if (isset($_POST['change_order'])) {
+if (isset($_POST["define_order"]) && !empty($_POST["col_order"])) {
+    $name = $_POST["col_order"];
+    $order = $_POST["row_order"];
 
-    $query = "SELECT First_Name, Last_Name FROM Doctor";
+    $query = "SELECT First_Name, Last_Name FROM Doctor ORDER BY $name $order";
     $result = mysqli_query($connection, $query);
+
+?>
+
+<tr style="background-color: lightblue;">
+    <th>First Name</th>
+    <th>Last Name</th>
+</tr>
+
+<?php
     
     while($row=mysqli_fetch_assoc($result)) {
     ?>
@@ -39,19 +46,28 @@ if (isset($_POST['change_order'])) {
     mysqli_free_result($result);
     mysqli_close($connection);
 }
+else if (empty($_POST["col_order"]) && empty($_POST["row_order"])){
+   // case where no radio buttons selected
+}
+else if (empty($_POST["col_order"])){
+   echo "<p style='color: red;'>" . "<b>Error: Select Column to Order By</b>" . "</p>";
+}
 
 ?>
 </table>
 <br>
 <form action="index.php" method="POST">
     <div>
-        <input type="radio" name="col_order"> Order By First Name
+        <h4>Order Columns By:</h4>
+        <input type="radio" name="col_order" value="First_Name"> Order By First Name
         <br>
-        <input type="radio" name="col_order"> Order By Last Name
+        <input type="radio" name="col_order" value="Last_Name"> Order By Last Name
         <br>
-        <input type="radio" name="row_order"> Ascending
+
+        <h4>Order Rows By:</h4>
+        <input type="radio" name="row_order" value="ASC"> Ascending
         <br>
-        <input type="radio" name="row_order"> Descending
+        <input type="radio" name="row_order" value="DESC"> Descending
         <br>
         <input type="submit" name="define_order">
     </div>
