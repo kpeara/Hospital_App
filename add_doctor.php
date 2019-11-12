d0f1ea3e312a5fc1d85131cd3f3ad69e2fd2b712
Kunjunnithan Giri Perath <kperath@uwo.ca>
2019-11-12 01:22:02 +0000

    added drop down menu to choose hospital codes from

---
+++ add_doctor.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HPDR</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+</head>
+<body>
+<?php include("templates/header.php"); ?>
+<div class="content">
+<h1>Add Doctor</h1>
+<?php
+
+include("dbconnect.php");
+
+if (isset($_POST["search_doctor"])) {
+// check if licence number is already in database
+
+    $date = $_GET["date"];
+
+    $query = "SELECT * FROM Doctor WHERE Licence_Date > '$date'";
+    $result = mysqli_query($connection, $query);
+    
+    if (!(mysqli_num_rows($result) == 0)) {
+    ?>
+        <tr style="background-color: lightblue;">
+            <th>Licence No.</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Specialty</th>
+            <th>Licence Date</th>
+            <th>Hospital Code</th>
+        </tr>
+        
+<?php
+        while($row=mysqli_fetch_assoc($result)) {
+    ?>
+        <tr>
+            <th><?php echo $row["Licence_No"] ?></th>
+            <th><?php echo $row["First_Name"] ?></th>
+            <th><?php echo $row["Last_Name"] ?></th>
+            <th><?php echo $row["Specialty"] ?></th>
+            <th><?php echo $row["Licence_Date"] ?></th>
+            <th><?php echo $row["Hospital_Code"] ?></th>
+        </tr>
+<?php
+        }
+    }
+    mysqli_free_result($result);
+}
+mysqli_close($connection);
+?>
+<form action="add_doctor.php" method="POST">
+    <div>
+	Licence Number:<input type="text" name="licenceno"><br>
+	First Name:<input type="text" name="firstname"><br>
+	Last Name:<input type="text" name="lastname"><br>
+	Specialty:<input type="text" name="specialty"><br>
+	Licence Date:<input type="text" name="licencedate"><br>
+	Hospital Code:<select name="hospitalname">
+	<?php
+
+	include("dbconnect.php");
+
+	$query = "SELECT Hospital_Code FROM Hospital;";
+	$result = mysqli_query($connection, $query);
+
+        while($row=mysqli_fetch_assoc($result)) {
+	    echo "<option value='" . $row["Hospital_Code"] . "'>" . $row["Hospital_Code"] . "</option>";
+	}
+	mysqli_close($connection);
+
+	?>
+	</select>
+	<input type="submit" name="add_doctor">
+    </div>
+</form>
+</body>
+</html>
